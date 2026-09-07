dca3f40ff3bc6a5ba7d08c36fb69092d644b0f4b
azeke <azekeprofit@gmail.com>
2026-09-08 00:27:29 +0500

    init

--- cfg/build.ts
+++ cfg/build.ts
@@ -1,15 +1,23 @@
-import { build } from "bun";
+import { $, build } from "bun";
 
-build({
-  entrypoints: ["lib/wrapper.tsx", "lib/background.ts"],
-  outdir: "public",
-  splitting: false,
-  minify: true,
-  metafile: { json: "meta.json" },
-  define: {
-    "process.env.NODE_ENV": '"production"',
-    "import.meta.env": "true",
-    "import.meta.env.MODE": '"production"',
-  },
-  plugins: [],
-}).catch(e=>console.log(`Error ${e}`))
+export async function bundle(prod) {
+  const { exitCode } = await $`bun res`.nothrow();
+  if (exitCode != 0) return;
+  build({
+    entrypoints: ["src/wrapper.jsx"], // , "lib/background.jsx"
+    outdir: "public",
+    splitting: false,
+    minify: false,
+    format: 'iife',
+    jsx: { importSource: 'preact' },
+    metafile: { json: "meta.json" },
+    define: {
+      "process.env.NODE_ENV": '"production"',
+      "import.meta.env": "true",
+      "import.meta.env.MODE": '"production"',
+    },
+    plugins: [],
+  }).catch(e => console.dir(e));
+}
+
+bundle(false)
